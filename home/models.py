edefd1ee8336adac85a6fd45d152a4095b1a0182
Jatin sharma <jatinsharma300802@gmail.com>
2022-01-28 19:17:09 +0530

    product category added

--- home/models.py
+++ home/models.py
@@ -11,6 +11,8 @@ class Customer(models.Model):
     phone = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True,blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    def __str__(self):
+        return self.name
 
 class ProductCategory(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
